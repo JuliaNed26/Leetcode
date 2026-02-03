25d9b2938c9cb60eb250713a245f3a841e9bbec5
JuliaNed26 <34402842+JuliaNed26@users.noreply.github.com>
2026-02-03 15:59:09 +0000

    Time: 25 ms (25.61%), Space: 62.6 MB (12.63%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cs
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cs
@@ -1,68 +1,88 @@
 public class Solution {
-    public IList<IList<int>> PacificAtlantic(int[][] heights) {
-        HashSet<(int x, int y)> toPacific = [];
-        HashSet<(int x, int y)> toAtlantic = [];
-        for(int x = 0; x < heights[0].Length; x++)
-        {
-            Bfs(x, 0, toPacific);
-        }
-        for(int y = 0; y < heights.Length; y++)
-        {
-            Bfs(0, y, toPacific);
-        }
-        for(int x = 0; x < heights[0].Length; x++)
-        {
-            Bfs(x, heights.Length - 1, toAtlantic);
-        }
-        for(int y = 0; y < heights.Length; y++)
-        {
-            Bfs(heights[0].Length - 1, y, toAtlantic);
-        }
+    public IList<IList<int>> PacificAtlantic(int[][] heights) 
+    {
+        var toPacific = GetReachPacific();
+        var toAtlantic = GetReachAtlantic();
 
-        var resultPoints = toPacific.Intersect(toAtlantic).ToList();
         List<IList<int>> result = [];
-        foreach(var point in resultPoints)
+        foreach(var point in toPacific.Intersect(toAtlantic))
         {
             result.Add([point.y, point.x]);
         }
+
         return result;
 
-
-        void Bfs(int x, int y, HashSet<(int x, int y)> toOcean)
+        HashSet<(int x, int y)> GetReachPacific()
         {
-            HashSet<(int x, int y)> seen = [];
-            var queue = new Queue<(int x, int y)>();
-            queue.Enqueue((x,y));
-            while(queue.Count != 0)
+            Queue<(int x, int y)> queue = [];
+
+            for(int i = 0; i < heights.Length; i++) 
             {
-                var curPoint = queue.Dequeue();
-                if (seen.Contains(curPoint))
-                {
-                    continue;
-                }
-                toOcean.Add(curPoint);
-                if (curPoint.x > 0
-                    && heights[curPoint.y][curPoint.x] <= heights[curPoint.y][curPoint.x-1])
-                {
-                    queue.Enqueue((curPoint.x - 1, curPoint.y));
-                }
-                if (curPoint.y > 0
-                    && heights[curPoint.y][curPoint.x] <= heights[curPoint.y - 1][curPoint.x])
-                {
-                    queue.Enqueue((curPoint.x, curPoint.y - 1));
-                }
-                if (curPoint.x < heights[0].Length - 1
-                    && heights[curPoint.y][curPoint.x] <= heights[curPoint.y][curPoint.x+1])
-                {
-                    queue.Enqueue((curPoint.x + 1, curPoint.y));
-                }
-                if (curPoint.y < heights.Length - 1
-                    && heights[curPoint.y][curPoint.x] <= heights[curPoint.y + 1][curPoint.x])
-                {
-                    queue.Enqueue((curPoint.x, curPoint.y + 1));
-                }
-                seen.Add(curPoint);
+                queue.Enqueue((0, i));
             }
+
+            for(int i = 0; i < heights[0].Length; i++) 
+            {
+                queue.Enqueue((i, 0));
+            }
+
+            return Bfs(queue, heights);
         }
+
+        HashSet<(int x, int y)> GetReachAtlantic()
+        {
+            Queue<(int x, int y)> queue = [];
+
+            for(int i = 0; i < heights.Length; i++) 
+            {
+                queue.Enqueue((heights[0].Length - 1, i));
+            }
+
+            for(int i = 0; i < heights[0].Length; i++) 
+            {
+                queue.Enqueue((i, heights.Length - 1));
+            }
+
+            return Bfs(queue, heights);
+        }
+    }
+
+    private HashSet<(int x, int y)> Bfs(Queue<(int x, int y)> queue, int[][] heights)
+    {
+        var visited = new HashSet<(int x, int y)>();
+
+        while(queue.Count > 0)
+        {
+            var curPoint = queue.Dequeue();
+            if(visited.Contains(curPoint))
+            {
+                continue;
+            }
+
+            if(curPoint.x > 0 
+                && heights[curPoint.y][curPoint.x - 1] >= heights[curPoint.y][curPoint.x])
+            {
+                queue.Enqueue((curPoint.x - 1, curPoint.y));
+            }
+            if(curPoint.y > 0 
+                && heights[curPoint.y - 1][curPoint.x] >= heights[curPoint.y][curPoint.x])
+            {
+                queue.Enqueue((curPoint.x, curPoint.y - 1));
+            }
+            if(curPoint.x < heights[0].Length - 1
+                && heights[curPoint.y][curPoint.x + 1] >= heights[curPoint.y][curPoint.x])
+            {
+                queue.Enqueue((curPoint.x + 1, curPoint.y));
+            }
+            if(curPoint.y < heights.Length - 1
+                && heights[curPoint.y + 1][curPoint.x] >= heights[curPoint.y][curPoint.x])
+            {
+                queue.Enqueue((curPoint.x, curPoint.y + 1));
+            }
+
+            visited.Add(curPoint);
+        }
+
+        return visited;
     }
 }
