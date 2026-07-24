76a6fc58b8369f35bdb2ae6a0ed3b07fcfe96ad0
JuliaNed26 <34402842+JuliaNed26@users.noreply.github.com>
2026-07-24 16:55:19 +0300

    Time: 250 ms (51.23%), Space: 63.5 MB (26.07%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cs
+++ 0743-network-delay-time/0743-network-delay-time.cs
@@ -2,24 +2,33 @@ public class Solution {
     private const int MAX_TIME_VALUE = 20000;
 
     public int NetworkDelayTime(int[][] times, int n, int k) {
+        Dictionary<int, List<(int Neighbour, int Time)>> graph = times.GroupBy(t => t[0])
+                        .ToDictionary(x => x.Key, x => x.Select(t => (t[1],t[2])).ToList());
         var distances = Enumerable.Repeat(MAX_TIME_VALUE, n + 1).ToArray();
+        var pq = new PriorityQueue<int, int>();
+        var visited = new HashSet<int>();
+        pq.Enqueue(k, 0);
         distances[k] = 0;
-        for(int i = 0; i < n; i++)
+        while(pq.Count > 0)
         {
-            var updated = false;
-            foreach(var time in times)
+            var curElement = pq.Dequeue();
+            if(visited.Contains(curElement))
             {
-                var newDist = distances[time[0]] + time[2];
-                if(newDist < distances[time[1]])
+                continue;
+            }
+            if(graph.TryGetValue(curElement, out var neighbours))
+            {
+                foreach(var neighbour in neighbours)
                 {
-                    distances[time[1]] = newDist;
-                    updated = true;
+                    var newDist = distances[curElement] + neighbour.Time;
+                    if(!visited.Contains(neighbour.Neighbour) && distances[neighbour.Neighbour] > newDist)
+                    {
+                        distances[neighbour.Neighbour] = newDist;
+                        pq.Enqueue(neighbour.Neighbour, newDist);
+                    }
                 }
             }
-            if(!updated)
-            {
-                break;
-            }
+            visited.Add(curElement);
         }
 
         var result = distances.Skip(1).Max();
