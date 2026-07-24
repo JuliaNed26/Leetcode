3ba472c23d940c9797832693735a5512b125226b
JuliaNed26 <34402842+JuliaNed26@users.noreply.github.com>
2026-07-24 16:23:20 +0300

    Time: 236 ms (83.74%), Space: 61.3 MB (76.69%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cs
+++ 0743-network-delay-time/0743-network-delay-time.cs
@@ -2,46 +2,27 @@ public class Solution {
     private const int MAX_TIME_VALUE = 20000;
 
     public int NetworkDelayTime(int[][] times, int n, int k) {
-        var graph = Enumerable.Range(0, n + 1).Select(_ =>
-                Enumerable.Repeat(MAX_TIME_VALUE, n + 1).ToArray())
-            .ToArray();
-
-        foreach(var time in times) 
+        var distances = Enumerable.Repeat(MAX_TIME_VALUE, n + 1).ToArray();
+        distances[k] = 0;
+        for(int i = 0; i < n; i++)
         {
-            graph[time[0]][time[1]] = time[2];
-        }
-
-        for(int i = 1; i <= n; i++)
-        {
-            graph[i][i] = 0;
-        }
-
-        for(int i = 1; i <= n; i++)
-        {
-            for(int y = 1; y <= n; y++)
+            var updated = false;
+            foreach(var time in times)
             {
-                if(y == i)
+                var newDist = distances[time[0]] + time[2];
+                if(newDist < distances[time[1]])
                 {
-                    continue;
+                    distances[time[1]] = newDist;
+                    updated = true;
                 }
-
-                for(int x = 1; x <= n; x++)
-                {
-                    if(y == x || x == i)
-                    {
-                        continue;
-                    }
-
-                    var curTime = graph[y][i] + graph[i][x];
-                    if(curTime < graph[y][x])
-                    {
-                        graph[y][x] = curTime;
-                    }
-                }
+            }
+            if(!updated)
+            {
+                break;
             }
         }
 
-        var maxSignalTime = graph[k].Skip(1).Max();
-        return maxSignalTime == MAX_TIME_VALUE ? -1 : maxSignalTime;
+        var result = distances.Skip(1).Max();
+        return result == MAX_TIME_VALUE ? -1 : result;
     }
 }
