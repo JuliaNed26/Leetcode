4d43cc605c6d6149398eff84fc9a047f7c4e7acb
JuliaNed26 <34402842+JuliaNed26@users.noreply.github.com>
2026-07-27 12:19:31 +0300

    Time: 115 ms (49.53%), Space: 60 MB (36.45%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cs
@@ -0,0 +1,56 @@
+public class Solution {
+    public int MinimumEffortPath(int[][] heights) {
+        var height = heights.Length;
+        var width = heights[0].Length;
+
+        var pq = new PriorityQueue<(int x, int y), int>();
+        var sides = new List<(int x, int y)> {
+            (1, 0),
+            (0, 1),
+            (-1, 0),
+            (0, -1)
+        };
+        pq.Enqueue((0,0), 0);
+        var efforts = Enumerable.Range(0, height)
+                                .Select(_ => Enumerable.Repeat(int.MaxValue, width).ToArray())
+                                .ToArray();
+        var visited = Enumerable.Range(0, height)
+                                .Select(_ => Enumerable.Repeat(false, width).ToArray())
+                                .ToArray();
+        while(pq.Count > 0)
+        {
+            pq.TryDequeue(out var curCell, out var curHeight);
+            if(visited[curCell.y][curCell.x])
+            {
+                continue;
+            }
+
+            visited[curCell.y][curCell.x] = true;
+            if(curCell.x == width - 1 && curCell.y == height - 1)
+            {
+                return curHeight;
+            }
+            if(curHeight < efforts[curCell.y][curCell.x])
+            {
+                efforts[curCell.y][curCell.x] = curHeight;
+            }
+
+            foreach(var side in sides)
+            {
+                var neighX = side.x + curCell.x;
+                var neighY = side.y + curCell.y;
+                if(neighX >= 0 && neighX < width && neighY >= 0 && neighY < height)
+                {
+                    var diff = Math.Abs(heights[curCell.y][curCell.x] - heights[neighY][neighX]);
+                    var effort = Math.Max(diff, curHeight);
+                    if(efforts[neighY][neighX] > effort)
+                    {
+                        pq.Enqueue((neighX, neighY), effort);
+                    }
+                } 
+            }
+        }
+
+        return efforts[height - 1][width - 1];
+    }
+}
