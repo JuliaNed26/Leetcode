80ec32220337a2181f8d0c7df723632bd10a7d41
JuliaNed26 <34402842+JuliaNed26@users.noreply.github.com>
2026-08-20 10:58:09 +0300

    Time: 19 ms (41.81%), Space: 52.1 MB (9.76%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cs
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cs
@@ -2,27 +2,20 @@ public class Solution {
     private int[][] _dp;
     
     public int LongestCommonSubsequence(string text1, string text2) {
-        _dp = Enumerable.Range(0, text1.Length)
-                        .Select(_ => Enumerable.Repeat(-1, text2.Length).ToArray())
+        _dp = Enumerable.Range(0, text1.Length + 1)
+                        .Select(_ => Enumerable.Repeat(0, text2.Length + 1).ToArray())
                         .ToArray();
 
-        return Dfs(text1, text2, 0, 0);
-    }
-
-    private int Dfs(string text1, string text2, int i, int j) {
-        if (i == text1.Length || j == text2.Length) {
-            return 0;
-        }
-        if(_dp[i][j] != -1)
+        for(int i = text1.Length - 1; i >= 0; i--)
         {
-            return _dp[i][j];
+            for(int j = text2.Length - 1; j >= 0; j--)    
+            {
+                _dp[i][j] = text1[i] == text2[j] 
+                            ? 1 + _dp[i + 1][j + 1]
+                            : Math.Max(_dp[i + 1][j], _dp[i][j + 1]);
+            }
         }
 
-        _dp[i][j] = text1[i] == text2[j]
-                    ? 1 + Dfs(text1, text2, i + 1, j + 1)
-                    : Math.Max(Dfs(text1, text2, i + 1, j),
-                        Dfs(text1, text2, i, j + 1)); 
-
-        return _dp[i][j];
+        return _dp[0][0];
     }
 }
