4b086434dd88861ebc241091af9e06b6c82a2726
JuliaNed26 <34402842+JuliaNed26@users.noreply.github.com>
2026-09-03 10:51:50 +0300

    Time: 5 ms (80.93%), Space: 41.5 MB (97.42%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cs
+++ 0518-coin-change-ii/0518-coin-change-ii.cs
@@ -1,25 +1,16 @@
 public class Solution {
 
     public int Change(int amount, int[] coins) {
-        Array.Sort(coins);
-        var dp = Enumerable.Range(0,2).Select(_ => new int[amount + 1]).ToArray();
-        for(int i = 0; i < 2; i++)
+        var dp = new int[amount + 1];
+        dp[0] = 1;
+        foreach (var coin in coins)
         {
-            dp[i][0] = 1;
-        }
-
-        for(int i = coins.Length - 1; i >= 0; i--)
-        {
-            for(int k = 1; k <= amount; k++)
+            for (var i = coin; i <= amount; i++)
             {
-                dp[0][k] = k < coins[i]
-                            ? 0
-                            : dp[1][k] + dp[0][k - coins[i]];
+                dp[i] += dp[i - coin];
             }
-
-            dp[0].CopyTo(dp[1], 0);
         }
 
-        return dp[0][amount];
+        return dp[amount];
     }
 }
